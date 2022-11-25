10e4f905d6ecb7af694be76f8ac1d6abdc945fcf
Jnv821 <jnv821@gmail.com>
2022-11-25 20:23:55 +0000

    Uploaded the whole code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let suitTop = document.querySelector(".suit-top");
+  let suitBottom = document.querySelector(".suit-bottom");
+  let cardValue = document.querySelector(".card-value");
+  let changeCardButton = document.querySelector(".change-card");
+
+  let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let suitValues = ["♣", "♥", "♠", "♦"];
+
+  function generateRandomValues() {
+    const generatedRandomSuit = Math.floor(Math.random() * suitValues.length);
+
+    suitTop.textContent = `${suitValues[generatedRandomSuit]}`;
+    suitBottom.textContent = `${suitValues[generatedRandomSuit]}`;
+
+    const generatedRandomValue = Math.floor(Math.random() * cardValues.length);
+    cardValue.textContent = `${cardValues[generatedRandomValue]}`;
+
+    if (generatedRandomSuit % 2 === 0) {
+      suitBottom.classList.add("black");
+      suitTop.classList.add("black");
+    } else {
+      suitBottom.classList.add("red");
+      suitTop.classList.add("red");
+      suitBottom.classList.remove("black");
+      suitTop.classList.remove("black");
+    }
+  }
+
+  generateRandomValues();
+
+  changeCardButton.addEventListener("click", generateRandomValues);
+
+  setTimeout(() => {
+    generateRandomValues();
+  }, 10000);
+
   console.log("Hello Rigo from the console!");
 };
